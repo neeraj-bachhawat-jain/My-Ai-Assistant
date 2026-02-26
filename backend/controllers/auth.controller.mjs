711ac88787aee6bea63c75057f9e2e986df299ba
Neeraj Jain <neeraj2003.jain@gmail.com>
2026-02-26 10:54:46 +0530

    Update cookie settings for authentication

--- backend/controllers/auth.controller.mjs
+++ backend/controllers/auth.controller.mjs
@@ -26,8 +26,8 @@ export const SignUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 1 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
 
     return res.status(201).send(user);
@@ -53,8 +53,8 @@ export const Login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
     });
 
     return res.status(200).send(user);
